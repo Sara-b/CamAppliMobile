79851659788bdf592d5b10525df86ad571ad14e9
Olivier Maurie <Olivier Maurie>
2016-11-04 16:21:15 +0100

    init log camera

--- www/js/controllers/dashboardCtrl.js
+++ www/js/controllers/dashboardCtrl.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers.DashboardCtrl', [])
-  .controller('DashboardCtrl', function ($http, $state, $scope, $stateParams, cameraService, userService, storageService) {
+  .controller('DashboardCtrl', function ($http, $state, $scope, $stateParams, cameraService, userService, storageService, logService) {
 
     $scope.dataStored = storageService.getStorage('data');
 
@@ -7,54 +7,57 @@ angular.module('starter.controllers.DashboardCtrl', [])
         $state.reload();
     }
     else {
-        $scope.user = JSON.parse($scope.dataStored);
-        var userid = $scope.user.user.id;
+      $scope.user = JSON.parse($scope.dataStored);
+      var userid = $scope.user.user.id;
 
-        var data = cameraService.getAll(userid);
-        data.then(function (response) {
+      var data = cameraService.getAll(userid);
+      data.then(function (response) {
 
         $scope.cameras = response.data;
+        for(i = 0; i < $scope.cameras.length; i++){
+          var isOwner;
+          var userId = $scope.cameras[i].user.id;
+          var cameraId = $scope.cameras[i].camera.owner;
+          var cameraSwitch = $scope.cameras[i].camera.switchOn;
 
-
-        
-          for(i = 0; i < $scope.cameras.length; i++){
-            
-            var isOwner;
-            var userId = $scope.cameras[i].user.id;
-            var cameraId = $scope.cameras[i].camera.owner;
-            var cameraSwitch = $scope.cameras[i].camera.switchOn;
-
-            // console.log($scope.cameras[i].camera);
-
-            // display admin settings button
-            if(userId == cameraId){
-              $scope.cameras[i].isOwner = true;
-            }else{
-              $scope.cameras[i].isOwner = false;
-            }
-
-            if(cameraSwitch == true){
-              $scope.cameras[i].color = "green";
-            }else{
-              $scope.cameras[i].color = "red";
-            }
-
-            // console.log(cameraSwitch + ' ' + $scope.class);
-
-            $scope.isOwner = $scope.cameras[i].isOwner;
+          // display admin settings button
+          if(userId == cameraId){
+            $scope.cameras[i].isOwner = true;
+          }else{
+            $scope.cameras[i].isOwner = false;
           }
 
-        // display on/off icon
+          // display on/off icon
+          if(cameraSwitch == true){
+            $scope.cameras[i].color = "green";
+          }else{
+            $scope.cameras[i].color = "red";
+          }
 
-        // console.log($scope.cameras);
+          $scope.isOwner = $scope.cameras[i].isOwner;
+        }
+
+        var logData = {
+          "user": userId,
+          "camera": cameraId,
+          "event": "Ouvre la caméra"
+        }
+
+        $scope.openCamera = function(){
+          logService.add(logData).then(function(response){console.log(response);});
+        }
 
           return $scope.cameras;
-        });
+      });
 
       $scope.addCamera = function(){
         $state.go('tab.addCamera');
       }
+
+
     }
+
+    
 
     $scope.openSettings = function (cameraid) {
         $state.go('tab.camera-settings', { camid: cameraid });
