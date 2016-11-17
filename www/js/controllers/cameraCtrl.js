d337a09834da9dda549853f8c67e23fd9746fe2d
Charles Diallo <Charles Diallo>
2016-11-17 17:10:23 +0100

    servomoteur websocket rotation OK

--- www/js/controllers/cameraCtrl.js
+++ www/js/controllers/cameraCtrl.js
@@ -1,37 +1,45 @@
 angular.module('starter.controllers.CameraCtrl', ['ngSails'])
-  .controller('CameraCtrl', function($state, $scope, $stateParams, cameraService,$sails){
-      
-    camera = cameraService.get($stateParams.id);
+    .controller('CameraCtrl', function ($state, $scope, $stateParams, cameraService, $sails) {
 
-    $scope.data = "";
+        camera = cameraService.get($stateParams.id);
 
-    camera.then(function(result){
-      console.log(result);
-        $scope.data = result;
-        $scope.camera = {
-            id: $scope.data.id,
-            name: $scope.data.name,
-            switchOn: $scope.data.switchOn,
-            owner: $scope.data.owner
+        $scope.data = "";
+        var socket = io("http://" + document.location.hostname + ":3000");
+
+
+        $scope.camera_turn = false;
+        camera.then(function (result) {
+            console.log('camera.result', result);
+            $scope.data = result;
+            $scope.camera = result;
+            $scope.camera.angle = 90;
+            $scope.camera_turn = true;
+        });
+
+        $sails.on("camera", function (message) {
+            switch (message.verb) {
+                case "updated" :
+                    $scope.camera = message.data[0]
+                    break;
+            }
+            console.log('youpi')
+            console.log(message)
+        });
+
+
+        $scope.switch = function () {
+            console.log("switch");
+            cameraService.update($scope.camera);
         };
+
+        $scope.turn = function (response) {
+            console.log("turn");
+            socket.emit('changeAngle', $scope.camera.angle);
+            console.log($scope.camera.angle);
+            socket.on('returnAng', function (ang) {
+                console.log(ang);
+            });
+        };
+
+
     });
-    
-     $sails.on("camera", function (message) {
-       switch(message.verb){
-          case "updated" : 
-         $scope.camera =  message.data[0]
-          break;
-       }
-      console.log('youpi')
-      console.log(message)
-    });
-
-
-    $scope.switch = function(){
-      console.log("switch");
-      cameraService.update($scope.camera);
-    }
-    
-    return $scope.camera;
-
-  });
--- www/js/controllers/dashboardCtrl.js
+++ www/js/controllers/dashboardCtrl.js
@@ -1,62 +1,61 @@
 angular.module('starter.controllers.DashboardCtrl', [])
-  .controller('DashboardCtrl', function ($http, $state, $scope, $stateParams, cameraService, userService, storageService) {
+    .controller('DashboardCtrl', function ($http, $state, $scope, $stateParams, cameraService, userService, storageService) {
 
-    $scope.dataStored = storageService.getStorage('data');
+        $scope.dataStored = storageService.getStorage('data');
 
-    if ($scope.dataStored == "") {
-        $state.reload();
-    }
-    else {
-        $scope.user = JSON.parse($scope.dataStored);
-        var userid = $scope.user.user.id;
+        if ($scope.dataStored == "") {
+            $state.reload();
+        }
+        else {
+            $scope.user = JSON.parse($scope.dataStored);
+            var userid = $scope.user.user.id;
 
-        var data = cameraService.getAll(userid);
-        data.then(function (response) {
+            var data = cameraService.getAll(userid);
+            data.then(function (response) {
 
-        $scope.cameras = response;
+                $scope.cameras = response;
 
 
-        
-          for(i = 0; i < $scope.cameras.length; i++){
-            
-            var isOwner;
-            var userId = $scope.cameras[i].user.id;
-            var cameraId = $scope.cameras[i].camera.owner;
-            var cameraSwitch = $scope.cameras[i].camera.switchOn;
+                for (i = 0; i < $scope.cameras.length; i++) {
 
-            // console.log($scope.cameras[i].camera);
+                    var isOwner;
+                    var userId = $scope.cameras[i].user.id;
+                    var cameraId = $scope.cameras[i].user;
+                    var cameraSwitch = $scope.cameras[i].switchOn;
 
-            // display admin settings button
-            if(userId == cameraId){
-              $scope.cameras[i].isOwner = true;
-            }else{
-              $scope.cameras[i].isOwner = false;
+                    // console.log($scope.cameras[i].camera);
+
+                    // display admin settings button
+                    if (userId == cameraId) {
+                        $scope.cameras[i].isOwner = true;
+                    } else {
+                        $scope.cameras[i].isOwner = false;
+                    }
+
+                    if (cameraSwitch == true) {
+                        $scope.cameras[i].color = "green";
+                    } else {
+                        $scope.cameras[i].color = "red";
+                    }
+
+                    // console.log(cameraSwitch + ' ' + $scope.class);
+
+                    $scope.isOwner = $scope.cameras[i].isOwner;
+                }
+
+                // display on/off icon
+
+                // console.log($scope.cameras);
+
+                return $scope.cameras;
+            });
+
+            $scope.addCamera = function () {
+                $state.go('tab.addCamera');
             }
+        }
 
-            if(cameraSwitch == true){
-              $scope.cameras[i].color = "green";
-            }else{
-              $scope.cameras[i].color = "red";
-            }
-
-            // console.log(cameraSwitch + ' ' + $scope.class);
-
-            $scope.isOwner = $scope.cameras[i].isOwner;
-          }
-
-        // display on/off icon
-
-        // console.log($scope.cameras);
-
-          return $scope.cameras;
-        });
-
-      $scope.addCamera = function(){
-        $state.go('tab.addCamera');
-      }
-    }
-
-    $scope.openSettings = function (cameraid) {
-        $state.go('tab.camera-settings', { camid: cameraid });
-    }
-});
+        $scope.openSettings = function (cameraid) {
+            $state.go('tab.camera-settings', {camid: cameraid});
+        }
+    });
