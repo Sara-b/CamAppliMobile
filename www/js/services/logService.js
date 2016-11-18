7e2e57ee0803bc04da5d5d9f20eb68892d042845
sara-b <sara.brin@y-nov.com>
2016-11-18 11:57:31 +0100

    websocket logs

--- www/js/services/logService.js
+++ www/js/services/logService.js
@@ -1,5 +1,5 @@
-angular.module('starter.services.logService', [])
-    .service('logService', function ($http, storageService) {
+angular.module('starter.services.logService', ['ngSails'])
+    .service('logService', function ($http, storageService, $sails, $q) {
 
         this.add = function (log) {
             return $http({
@@ -24,19 +24,23 @@ angular.module('starter.services.logService', [])
         }
 
         this.getByCamera = function (camId) {
-            return $http({
+            var defered = $q.defer();
+            $sails.request({
                 method: 'GET',
                 url: 'http://127.0.0.1:1337/log/camera/' + camId,
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                     'Authorization': 'JWT ' + storageService.getStorage('token')
-                },
-                dataType: 'json',
-                crossDomain: 'true'
-            }).then(function (response) {
-                return response;
+                }
+            }, function (resData, jwres) {
+                if (jwres.error) {
+                    return defered.reject(jwres.error);
+                }
+                return defered.resolve(resData);
             });
-        }
+
+            return defered.promise;
+        };
 
 
     });
